f6541129ed8f9d304cf792786bdcc32978f18eab
yongqiming <yongqiming@rongcloud.cn>
2025-09-19 09:49:57 +0800

    修改演示图片

--- config/RCConfig.js
+++ config/RCConfig.js
@@ -5,28 +5,28 @@
 
 export default {
 	// 融云AppKey - 请替换为您的实际AppKey
-	appKey: 'p5tvi9dspl334',
+	appKey: '',
 	
 	// 测试用户列表 - 可以方便地添加更多用户
 	testUsers: [
 		{
-			id: '9991',
+			id: '',
 			name: '测试用户1',
-			token: 'wRRcc7tpneznfWJ8b8iAmJnIhp/u8YXKOn8cO1FsaNg=@sdkq.cn.rongnav.com;sdkq.cn.rongcfg.com',
+			token: '',
 			avatar: '👤',
 			description: '主要测试账户'
 		},
 		{
-			id: '9992',
+			id: '',
 			name: '测试用户2', 
-			token: 'qYJ98NqYbwbnfWJ8b8iAmEbAJuTn6E8ryqmxM7wQQQs=@sdkq.cn.rongnav.com;sdkq.cn.rongcfg.com',
+			token: '',
 			avatar: '👤',
 			description: '备用测试账户'
 		},
 		{
-			id: '9993',
+			id: '',
 			name: '测试用户3', 
-			token: '3qPc8BJiV6jnfWJ8b8iAmI6cLFTpPUM9DjFo0WjC6Aw=@sdkq.cn.rongnav.com;sdkq.cn.rongcfg.com',
+			token: '',
 			avatar: '👤',
 			description: '备用测试账户'
 		}
